00a76ea6d0b3756f87e0197780f304bda5f3fad9
Alejandro Romero <alejo8591@gmail.com>
2013-10-15 21:50:19 -0500

    update admin file for especifications

--- grimm/users/admin.py
+++ grimm/users/admin.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from users.models import Student
+from users.models import Person
 
 class UserInline(admin.StackedInline):
-	model = Student
+	model = Person
 	can_delete = False
-	verbose_name=u'Código del Estudiante'
-	verbose_name_plural = u'estudiante'
+	verbose_name=u'Código del Estudiante o Profesor'
+	verbose_name_plural = u'estudiante o Profesor'
 
 class UserAdmin(UserAdmin):
 	inlines = (UserInline, )
