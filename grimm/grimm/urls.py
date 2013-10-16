f1ea55d5708c4b4335c489d2df3e28ec4c188596
Alejandro Romero <alejo8591@gmail.com>
2013-10-16 00:23:38 -0500

    update urls with support for DJANGO WYSIWYG

--- grimm/grimm/urls.py
+++ grimm/grimm/urls.py
@@ -15,4 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     ('^info/', include('django.contrib.flatpages.urls')),
+    # integrated wysiwyg
+    (r'^tinymce/', include('tinymce.urls')),
 )
